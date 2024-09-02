5f6c5de189ddd879c706d29c18f246ba5331be04
Rahul7486 <rahulgupta7486@gmail.com>
2024-09-02 21:33:01 +0530

    scripted pipeline code added

---
+++ jenkinsfile2.groovy
@@ -0,0 +1,29 @@
+pipeline {
+    agent any
+    // options {
+    //     skipStagesAfterUnstable()
+    // }
+    stages {
+        stage('Pull') {
+            steps {
+                echo 'code pulled successfully'
+            }
+        }
+        stage('Build') {
+            steps {
+                echo 'code build successfully'
+            }
+        }
+        stage('Test'){
+            steps {
+                echo 'code test successfully'
+                
+            }
+        }
+        stage('Deploy') {
+            steps {
+                echo 'code deployed successfully'
+            }
+        }
+    }
+}
